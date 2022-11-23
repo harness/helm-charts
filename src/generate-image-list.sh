b9fef3bbfecbd482b9f7aa6281dbf52695332d6f
Kapil Garg <kapilgarg1996@gmail.com>
2022-11-23 22:46:06 +0530

    add images.txt in pre-commit hook to auto-generate

--- src/generate-image-list.sh
+++ src/generate-image-list.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-helm template ./harness/ | grep docker.io | sort -u  | sed 's/^[^:]*: //g' | sed -e 's/^[ \t]*//' > images.txt
+SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+
+helm template ${SCRIPT_DIR}/harness | grep docker.io | sort -u  | sed 's/^[^:]*: //g' | sed -e 's/^[ \t]*//' > src/harness/images.txt
+
+exit 0
