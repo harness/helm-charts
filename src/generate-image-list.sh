8858f7bd5f74213a348e2276caf2b9470bc38d7f
Arya Haldar <arya.haldar@harness.io>
2022-12-01 23:51:51 +0530

    directly pulling from source charts

--- src/generate-image-list.sh
+++ src/generate-image-list.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-helm template ${SCRIPT_DIR}/harness | grep docker.io | sort -u  | sed 's/^[^:]*: //g' | sed -e 's/^[ \t]*//' > src/harness/images.txt
+helm template ${SCRIPT_DIR}/harness | grep docker.io | sort -u  | sed 's/^[^:]*: //g' | sed -e 's/^[ \t]*//' > harness/images.txt
 
 exit 0
