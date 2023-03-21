32115a86e32429ac8b564d8852d634bdec48156a
arya-harness <arya.haldar@harness.io>
2023-03-21 16:29:02 +0530

    Release/0.4.4 (#250)
    * [PIE-7712]: Ingress SMP for PMS
    
    * [PIE-7712]: Templates
    
    * pulled latest charts
    
    * smtp secret support for harness-manager
    
    * feat[CHAOS-785]: add Chaos module support in SMP (#233)
    
    * feat[CHAOS-785]: add Chaos module support in SMP
    
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>
    
    * revert chart.yaml changes
    
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>
    
    * resolve conflicts
    
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>
    
    ---------
    
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>
    
    * smp-811: Add warnings for skipped releases (#236)
    
    * Add notes for release warning
    
    * Add lookup function to check for releases
    
    * fixed notes.txt
    
    * fix notes.txt
    
    * fix virtual service
    
    * Branch cut for release 0.4.0
    
    * Pre-release Dep updates
    
    * Pre-release Dep updates
    
    * Update image list
    
    * Release Chart with updated images
    
    * Updating CI charts
    
    * Updated CI builds
    
    * updated image list
    
    * Bumping chart version to 0.4.3
    
    * Update dashboards overrides to only include customer important config
    
    * SMP-795: Hotfix for disabling secrets for USAF
    
    * Bump chart version
    
    * revise documentation
    
    * Updated charts, added option to disable secrets, new image.txt
    
    ---------
    
    Signed-off-by: Sagar Kumar <sagar.kumar@harness.io>
    Co-authored-by: Mankrit Singh <mankrit.singh@harness.io>
    Co-authored-by: Sagar Kumar <sagar.kumar@harness.io>
    Co-authored-by: kapil garg <kapilgarg1996@users.noreply.github.com>
    Co-authored-by: CI Bot <bot@harness.io>
    Co-authored-by: Seán Dunne <sean.dunne@harness.io>

--- src/generate-image-list.sh
+++ src/generate-image-list.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-helm template ${SCRIPT_DIR}/harness -f ${SCRIPT_DIR}/generate-image.yaml | grep docker.io | sort -u  | sed 's/^[^:]*: //g' | sed -e 's/^[ \t]*//' | sed -e 's/^"//' -e 's/"$//' > src/harness/images.txt
+helm template ${SCRIPT_DIR}/harness -f  ${SCRIPT_DIR}/generate-image.yaml | grep -i image | grep \/ | grep -v imagePullPolicy | grep -v "#" | awk '{$1=$1};1' | sort -u | sed 's/^[^:]*: //g' | sed -e "s/^'//" -e "s/'$//"| sed -e 's/^"//' -e 's/"$//' > src/harness/images.txt
 
 exit 0
