01794c35e62e81e3385ef309f571f25e5ca9b373
reetika04 <mallavarapu.reetika@harness.io>
2023-04-17 14:52:14 +0530

    fix: Process docker pull in parallel

--- src/harness/scripts/save-images.sh
+++ src/harness/scripts/save-images.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
 images="harness-airgapped-images.tar.gz"
-list="images.txt"
+list="airgapped-images.txt"
 
-while IFS= read -r i; do 
-    [ -z "${i}" ] && continue
-    if [ $(docker pull --quiet "${i}") ]; then
+pull_image() {
+    i="$1"
+    if docker pull --quiet "${i}"; then
         echo "Image pull success: ${i}"
-        pulled="${pulled} ${i}"
+        echo "${i}" >> "${pulled_file}"
     else
         echo "Image pull failed: ${i}"
     fi
+}
+
+# Create a temporary file to store the list of successfully pulled images
+pulled_file="$(mktemp)"
+
+# Download images in parallel
+while IFS= read -r i; do
+    [ -z "${i}" ] && continue
+    pull_image "${i}" &
 done < "${list}"
 
+# Wait for all background tasks to finish
+wait
+
+# Get the list of successfully pulled images
+pulled=$(cat "${pulled_file}")
+
+# Save pulled images to a tarball
 echo "Creating ${images} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
 docker save $(echo ${pulled}) | gzip --stdout > ${images}
+
+# Remove temporary file
+rm "${pulled_file}"
