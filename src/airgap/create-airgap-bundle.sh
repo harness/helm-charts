2eff1f789ec651b12422f7e3789f4470ec187698
Swapnil Deshmukh <swapnil.deshmukh@harness.io>
2023-10-30 22:30:42 -0700

    update

--- src/airgap/create-airgap-bundle.sh
+++ src/airgap/create-airgap-bundle.sh
@@ -9,6 +9,7 @@ handle_error() {
 
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
+# Provide lists of image names
 lists=("cdng_images.txt" "ci_images.txt" "platform_images.txt" "ccm_images.txt" 
 "ce_images.txt" "sto_images.txt" "cet_images.txt" "ff_images.txt")
 
@@ -22,13 +23,50 @@ pull_image() {
     fi
 }
 
+# Check if an image name parameter is provided
 if [ $# -eq 1 ]; then
-    image_name="$1"
-    images_file="${image_name}.tgz"
-    list_file=""
-    if ! docker pull --quiet "${image_name}"; then
-        handle_error "Image pull failed: ${image_name}"
+    image_list_file="$1"
+    if [[ ! -f $image_list_file ]]; then
+        echo "Error: File $image_list_file not found."
+        exit 1
     fi
+
+    # Generate image file name from the list name
+    base_name=$(basename "$image_list_file" .txt)
+    images_file="${base_name}.tgz"
+    
+    # Create a temporary file to store the list of successfully pulled images
+    pulled_file="$(mktemp)"
+
+    pids=()
+
+    # Download images in parallel
+    while IFS= read -r i; do
+        [ -z "${i}" ] && continue
+        pull_image "${i}" &
+        pids+=($!)
+    done < "${image_list_file}"
+
+    for pid in ${pids[*]}; do
+        wait $pid || handle_error "Failed background task with PID: $pid"
+    done
+    # Wait for all background tasks to finish
+    wait
+
+    # Get the list of successfully pulled images
+    pulled=$(cat "${pulled_file}")
+
+    # Save pulled images to a tarball
+    echo "Creating ${images_file} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
+    docker save $(echo ${pulled}) | gzip --stdout > ${images_file} || handle_error "Failed to create tarball: ${images_file}"
+
+    # Remove temporary file
+    rm "${pulled_file}" || handle_error "Failed to remove temporary file: ${pulled_file}"
+
+    # Cleanup: Remove the pulled images
+    for image in ${pulled}; do
+        docker rmi -f ${image}
+    done
 else
     # Loop through each list
     for list in ${lists[*]}; do
@@ -60,13 +98,17 @@ else
         # Wait for all background tasks to finish
         wait
 
+        # Get the list of successfully pulled images
         pulled=$(cat "${pulled_file}")
 
+        # Save pulled images to a tarball
         echo "Creating ${images_file} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
         docker save $(echo ${pulled}) | gzip --stdout > ${images_file} || handle_error "Failed to create tarball: ${images_file}"
 
+        # Remove temporary file
         rm "${pulled_file}" || handle_error "Failed to remove temporary file: ${pulled_file}"
 
+        # Cleanup: Remove the pulled images
         for image in ${pulled}; do
             docker rmi -f ${image}
         done
