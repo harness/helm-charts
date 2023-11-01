90bf4384225128c421b4847d3c3f3b0399f96714
Swapnil Deshmukh <swapnil.deshmukh@harness.io>
2023-11-01 14:50:57 -0700

    Added an option to specify individual bundle creation

--- src/airgap/create-airgap-bundle.sh
+++ src/airgap/create-airgap-bundle.sh
@@ -23,44 +23,93 @@ pull_image() {
     fi
 }
 
+if [ $# -eq 1 ]; then
+    image_list_file="$1"
+    if [[ ! -f $image_list_file ]]; then
+        echo "Error: File $image_list_file not found."
+        exit 1
+    fi
+
+    base_name=$(basename "$image_list_file" .txt)
+    images_file="${base_name}.tgz"
+    
+    # Create a temporary file to store the list of successfully pulled images
+    pulled_file="$(mktemp)"
+
+    pids=()
+
+    # Download images in parallel
+    while IFS= read -r i; do
+        [ -z "${i}" ] && continue
+        pull_image "${i}" &
+        pids+=($!)
+    done < "${image_list_file}"
+
+    for pid in ${pids[*]}; do
+        wait $pid || handle_error "Failed background task with PID: $pid"
+    done
+    # Wait for all background tasks to finish
+    wait
+
+    # Get the list of successfully pulled images
+    pulled=$(cat "${pulled_file}")
+
+    # Save pulled images to a tarball
+    echo "Creating ${images_file} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
+    docker save $(echo ${pulled}) | gzip --stdout > ${images_file} || handle_error "Failed to create tarball: ${images_file}"
+
+    # Remove temporary file
+    rm "${pulled_file}" || handle_error "Failed to remove temporary file: ${pulled_file}"
+
+    # Cleanup: Remove the pulled images
+    #for image in ${pulled}; do
+    #    docker rmi -f ${image}
+    #done
+
+else
 # Loop through each list
-for list in ${lists[*]}; do
+  for list in ${lists[*]}; do
+    if [[ ! -f $list ]]; then
+      echo "Error: File $list not found."
+      exit 1
+    fi
 
-  if [[ ! -f $list ]]; then
-    echo "Error: File $list not found."
-    exit 1
-  fi
+    # Generate image file name from list name
+    base_name=$(basename "$list" .txt)
+    images_file="${base_name}.tgz"
 
-  # Generate image file name from list name
-  base_name=$(basename "$list" .txt)
-  images_file="${base_name}.tgz"
+    # Create a temporary file to store the list of successfully pulled images
+    pulled_file="$(mktemp)"
 
-  # Create a temporary file to store the list of successfully pulled images
-  pulled_file="$(mktemp)"
+    pids=()
+    
+    # Download images in parallel
+    while IFS= read -r i; do
+        [ -z "${i}" ] && continue
+        pull_image "${i}" &
+        pids+=($!)
+    done < "${list}"
 
-  pids=()
-  
-  # Download images in parallel
-  while IFS= read -r i; do
-      [ -z "${i}" ] && continue
-      pull_image "${i}" &
-      pids+=($!)
-  done < "${list}"
+    for pid in ${pids[*]}; do
+      wait $pid || handle_error "Failed background task with PID: $pid"
+    done
+    # Wait for all background tasks to finish
+    wait
 
-  for pid in ${pids[*]}; do
-    wait $pid || handle_error "Failed background task with PID: $pid"
+    # Get the list of successfully pulled images
+    pulled=$(cat "${pulled_file}")
+
+    # Save pulled images to a tarball
+    echo "Creating ${images_file} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
+    docker save $(echo ${pulled}) | gzip --stdout > ${images_file} || handle_error "Failed to create tarball: ${images_file}"
+
+
+    # Remove temporary file
+    rm "${pulled_file}" || handle_error "Failed to remove temporary file: ${pulled_file}"
+    Cleanup: Remove the pulled images
+    for image in ${pulled}; do
+            docker rmi -f ${image}
+    done
+    
   done
-  # Wait for all background tasks to finish
-  wait
-
-  # Get the list of successfully pulled images
-  pulled=$(cat "${pulled_file}")
-
-  # Save pulled images to a tarball
-  echo "Creating ${images_file} with $(echo ${pulled} | wc -w | tr -d '[:space:]') images"
-  docker save $(echo ${pulled}) | gzip --stdout > ${images_file} || handle_error "Failed to create tarball: ${images_file}"
-
-
-  # Remove temporary file
-  rm "${pulled_file}" || handle_error "Failed to remove temporary file: ${pulled_file}"
-done
+fi
