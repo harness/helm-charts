a07d4fac52133e3c9635a6c3cfa213c999c9e8d7
arya-harness <arya.haldar@harness.io>
2023-12-11 20:21:14 +0530

    Release/0.11.0 (#490)
    * sync service versions
    
    * sync service versions
    
    * sync service versions
    
    * fix: [SMP-2326]: Remove hardcoded image tags in values.yaml for nginx and update ffs
    
    * SMP-2336: Move/Remove NEXT_GEN_ENABLED, ENABLE_DEFAULT_NG_EXPERIENCE_FOR_ONPREM flags from Base to NG (#469)
    
    * update modules dependencies (#472)
    
    * update umbrella dependencies (#473)
    
    * Update images.txt : Add anchore image (#474)
    
    * Update airgap_input.txt: Rename smp-chaos-subscriber-signed (#475)
    
    * Update generate-image.yaml: ADD SSCA (#476)
    
    * Update images.txt: Add SSCA images (#477)
    
    * Update images.txt: Add anchore job runner (#478)
    
    * Update airgap_input.txt: Add new images (#479)
    
    * added validation after image push
    
    * increment umbrella chart patch version
    
    * sync service versions
    
    * sync service versions
    
    * update modules dependencies (#481)
    
    Co-authored-by: bot-harness <bot@harness.io>
    
    * update umbrella dependencies (#482)
    
    Co-authored-by: bot-harness <bot@harness.io>
    
    * Update images.txt (#483)
    
    * increment umbrella chart patch version
    
    * sync service versions
    
    * update modules dependencies (#486)
    
    Co-authored-by: bot-harness <bot@harness.io>
    
    * update umbrella dependencies (#487)
    
    * update umbrella dependencies
    
    * add anchore job runner in images.txt
    
    ---------
    
    Co-authored-by: bot-harness <bot@harness.io>
    Co-authored-by: Kapil Garg <kapilgarg1996@gmail.com>
    
    ---------
    
    Co-authored-by: bot-harness <bot@harness.io>
    Co-authored-by: saitejavn-harness <134731283+saitejavn-harness@users.noreply.github.com>
    Co-authored-by: kapil garg <kapilgarg1996@users.noreply.github.com>
    Co-authored-by: Swapnil Deshmukh <swapnil.deshmukh@harness.io>
    Co-authored-by: bot2@harness.io <bot2@harness.io>
    Co-authored-by: bot2-harness <152529449+bot2-harness@users.noreply.github.com>
    Co-authored-by: Kapil Garg <kapilgarg1996@gmail.com>

--- src/airgap/harness-airgap-images.sh
+++ src/airgap/harness-airgap-images.sh
@@ -4,6 +4,11 @@
 registry=""
 tgz_file=""
 tgz_directory=""
+success_count=0
+fail_count=0
+declare -a failed_images
+declare -a missing_images
+declare -a verified_images
 
 # Function to display help message
 function show_help {
@@ -14,9 +19,19 @@ function show_help {
   exit 1
 }
 
+check_image_in_registry() {
+  local image=$1
+  if docker manifest inspect "$image" > /dev/null 2>&1; then
+    return 0  # Image exists
+  else
+    return 1  # Image does not exist
+  fi
+}
+
 # Parse command-line arguments
-while getopts "r:f:d:" opt; do
+while getopts "hr:f:d:" opt; do
   case "$opt" in
+    h) show_help ;;
     r) registry="$OPTARG" ;;
     f) tgz_file="$OPTARG" ;;
     d) tgz_directory="$OPTARG" ;;
@@ -24,57 +39,87 @@ while getopts "r:f:d:" opt; do
   esac
 done
 
-if [ -z "$tgz_file" ] && [ -z "$tgz_directory" ]; then
-  show_help
-fi
-
+# Check for mandatory options
 if [ -z "$registry" ]; then
+  echo "Registry not specified!"
   show_help
+  exit 1
 fi
 
-if [ -n "$tgz_file" ]; then
-  echo "Processing $tgz_file"
-
-  tmp_file=$(mktemp)
-
-  docker load -i "$tgz_file" > "$tmp_file"
-
-  while read -r image_info; do
-    image_info="${image_info#Loaded image: }"
-    if [ -n "$image_info" ]; then
-      docker tag "$image_info" "$registry/$image_info" && echo "Tagged: $registry/$image_info"
-      docker push "$registry/$image_info" && echo "Pushed: $registry/$image_info"
-    else
-      echo "Failed to extract image info from load output."
-    fi
-  done < "$tmp_file"
-
-  rm "$tmp_file"
-
-  echo "Finished processing $tgz_file"
+if [ -z "$tgz_file" ] && [ -z "$tgz_directory" ]; then
+  echo "No .tgz file or directory specified!"
+  show_help
+  exit 1
 fi
 
-if [ -n "$tgz_directory" ]; then
-  for tgz_file in "$tgz_directory"/*.tgz; do
-    echo "Processing $tgz_file"
+process_tgz_file() {
+  local file=$1
+  echo "Processing $file..."
+  local image_info
+  local load_output
 
-    tmp_file=$(mktemp)
-
-    docker load -i "$tgz_file" > "$tmp_file"
-
-    while read -r image_info; do
-      image_info="${image_info#Loaded image: }"
-      if [ -n "$image_info" ]; then
-        docker tag "$image_info" "$registry/$image_info" && echo "Tagged: $registry/$image_info"
-        docker push "$registry/$image_info" && echo "Pushed: $registry/$image_info"
+  # Load the Docker image from a .tgz file
+  load_output=$(docker load -i "$file" 2>&1)
+  
+  # Extract the image names and tags
+  while IFS= read -r line; do
+    if [[ "$line" =~ Loaded\ image:\ (.+) ]]; then
+      image_info="${BASH_REMATCH[1]}"
+      echo "Loaded image: $image_info"
+      
+      # Tag the image with the provided registry
+      if docker tag "$image_info" "$registry/$image_info"; then
+        # Push the image to the registry
+        if docker push "$registry/$image_info"; then
+          echo "Successfully pushed $image_info to $registry"
+          ((success_count++))
+          verified_images+=("$image_info")
+        else
+          echo "Failed to push $image_info to $registry"
+          failed_images+=("$image_info")
+          ((fail_count++))
+        fi
       else
-        echo "Failed to extract image info from load output."
+        echo "Failed to tag $image_info with $registry"
+        failed_images+=("$image_info")
+        ((fail_count++))
       fi
-    done < "$tmp_file"
+    fi
+  done <<< "$load_output"
+}
 
-    rm "$tmp_file"
-
-    echo "Finished processing $tgz_file"
+# Process the specified .tgz file or directory
+if [[ -n "$tgz_file" ]]; then
+  process_tgz_file "$tgz_file"
+elif [[ -n "$tgz_directory" ]]; then
+  for file in "$tgz_directory"/*.tgz; do
+    process_tgz_file "$file"
   done
+fi
+
+# Verify all pushed images are in the registry
+for image in "${verified_images[@]}"; do
+  if ! check_image_in_registry "$registry/$image"; then
+    echo "Image missing in registry: $registry/$image"
+    missing_images+=("$image")
+  fi
+done
+
+# Print statistics
+echo "Total successful pushes: $success_count"
+echo "Total failed pushes: $fail_count"
+if [[ ${#failed_images[@]} -gt 0 ]]; then
+  echo "Failed images:"
+  for image in "${failed_images[@]}"; do
+    echo "$image"
+  done
+fi
+if [[ ${#missing_images[@]} -gt 0 ]]; then
+  echo "Images missing in registry after push: ${#missing_images[@]}"
+  for image in "${missing_images[@]}"; do
+    echo "$image"
+  done
+else
+  echo "All pushed images verified in registry."
 fi
 
