fdb1fcc19ca47c1ef1365bfaada20fa8066f8911
Akash Shrivastava <akash.shrivastava@harness.io>
2024-09-03 11:06:16 +0530

    improving script for different versions
    Signed-off-by: Akash Shrivastava <akash.shrivastava@harness.io>

--- src/harness/scripts/update-ingress-objects.sh
+++ src/harness/scripts/update-ingress-objects.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-# Function to print usage information
-usage() {
-  echo "Usage: $0 <namespace>"
-  exit 1
-}
-
-# Check if correct number of arguments is provided
-if [ $# -ne 1 ]; then
-  usage
-fi
-
+read -p "Enter Namespace: " NAMESPACE
+read -p "Enter Harness Upgrade version (eg: 0.20.0): " VERSION
 # Assign input argument to variable
-NAMESPACE=$1
 
-# Arrays for old and new Ingress names
-OLD_INGRESS_NAMES=("log-service" "pipeline-service-smp-v1-apis" "access-control" "ci-manager" "template-service" "ssca-manager" "ssca-manager-smp-v1-apis")
-NEW_INGRESS_NAMES=("log-service-0" "pipeline-service-v1-apis" "access-control-service" "ci-manager-0" "template-service-0" "ssca-manager-0" "ssca-manager-v1-apis")
+# Perform actions based on the version
+if [[ "$VERSION" == "0.20."* ]]; then
+  OLD_INGRESS_NAMES=("log-service" "pipeline-service-smp-v1-apis" "access-control" "ci-manager" "template-service" "ssca-manager" "ssca-manager-smp-v1-apis")
+  NEW_INGRESS_NAMES=("log-service-0" "pipeline-service-v1-apis" "access-control-service" "ci-manager-0" "template-service-0" "ssca-manager-0" "ssca-manager-v1-apis")
+
+elif [[ "$VERSION" == "0.19."* ]]; then
+  OLD_INGRESS_NAMES=("pipeline-service-smp-v1-apis" "access-control" "ci-manager" "template-service" "ssca-manager" "ssca-manager-smp-v1-apis")
+  NEW_INGRESS_NAMES=("pipeline-service-v1-apis" "access-control-service" "ci-manager-0" "template-service-0" "ssca-manager-0" "ssca-manager-v1-apis")
+
+elif [[ "$VERSION" == "0.18."* ]]; then
+  OLD_INGRESS_NAMES=("pipeline-service-smp-v1-apis" "template-service" "ssca-manager" "ssca-manager-smp-v1-apis")
+  NEW_INGRESS_NAMES=("pipeline-service-v1-apis" "template-service-0" "ssca-manager-0" "ssca-manager-v1-apis")
+
+elif [[ "$VERSION" == "0.17."* ]]; then
+  OLD_INGRESS_NAMES=("pipeline-service-smp-v1-apis" "template-service" "ssca-manager" "ssca-manager-smp-v1-apis")
+  NEW_INGRESS_NAMES=("pipeline-service-v1-apis" "template-service-0" "ssca-manager-0" "ssca-manager-v1-apis")
+
+else
+  echo "Not required to run for version $VERSION"
+  exit 1
+fi
 
 # Loop through the arrays and process each Ingress
 for i in "${!OLD_INGRESS_NAMES[@]}"; do
