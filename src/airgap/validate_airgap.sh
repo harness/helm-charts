d38fbbd9d73623972ba84d97c993cf27f1c7d61a
siraj-io <siraj.khan@harness.io>
2025-03-10 16:55:23 +0530

    remove whitespaces

--- src/airgap/validate_airgap.sh
+++ src/airgap/validate_airgap.sh
@@ -33,7 +33,7 @@ for module_name in "${MODULE_NAMES[@]}"; do
 
     while IFS= read -r line; do
         line_without_prefix="${line#docker.io/}"
-        
+
         if [[ "$IMAGES_LIST" =~ "$line" || "$IMAGES_LIST" =~ "$line_without_prefix" ]]; then
             echo "$line"
         else
