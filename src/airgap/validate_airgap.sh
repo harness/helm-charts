d3468d4fecb9c690df55be773edf3d7d21ff2f84
siraj-io <siraj.khan@harness.io>
2025-03-10 16:52:54 +0530

    remove whitespaces

--- src/airgap/validate_airgap.sh
+++ src/airgap/validate_airgap.sh
@@ -18,32 +18,32 @@ for module_name in "${MODULE_NAMES[@]}"; do
     IMAGES_LIST=$(tar -xzOf "${TGZ_FILE}" manifest.json | jq -r '.[].RepoTags | join("\n")')
 
     [[ ! -f "${TXT_FILE}" ]] && abort "${TXT_FILE} not found!"
-    
+
     echo "Matching images for ${TGZ_FILE} and ${TXT_FILE}:"
-    
+
     mismatched=false
-    
+
     # Count the number of images in .tgz and .txt
     num_images_tgz=$(echo "$IMAGES_LIST" | wc -l)
     num_images_txt=$(wc -l < "${TXT_FILE}")
-    
+
     if [ "$num_images_tgz" -ne "$num_images_txt" ]; then
-       abort "Number of images in ${TGZ_FILE} does not match ${TXT_FILE}"
+        abort "Number of images in ${TGZ_FILE} does not match ${TXT_FILE}"
     fi
-    
+
     while IFS= read -r line; do
-       line_without_prefix="${line#docker.io/}"
+      line_without_prefix="${line#docker.io/}"
     
-       if [[ "$IMAGES_LIST" =~ "$line" || "$IMAGES_LIST" =~ "$line_without_prefix" ]]; then
-           echo "$line"
-       else
-           mismatched=true
-           echo "Mismatch found: $line"
-       fi
+      if [[ "$IMAGES_LIST" =~ "$line" || "$IMAGES_LIST" =~ "$line_without_prefix" ]]; then
+          echo "$line"
+      else
+          mismatched=true
+          echo "Mismatch found: $line"
+      fi
     done < "${TXT_FILE}"
-    
+
     if $mismatched; then
-       abort "Images in ${TGZ_FILE} and ${TXT_FILE} do not match"
+        abort "Images in ${TGZ_FILE} and ${TXT_FILE} do not match"
     fi
 
     echo "--------------------------------"
